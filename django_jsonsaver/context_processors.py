77ee2ff5e375ef6c9b72a1f4fbc72ccfe8799ed2
Nicholas Moen <arcanemachine@tutanota.com>
2021-02-24 18:49:07 -0700

    Decrease line count

--- django_jsonsaver/context_processors.py
+++ django_jsonsaver/context_processors.py
@@ -2,7 +2,5 @@ from django_jsonsaver import server_config as sc
 
 
 def constants(request):
-    return {
-        'PROJECT_NAME': sc.PROJECT_NAME,
-        'BACKEND_SERVER_URL': sc.BACKEND_SERVER_URL
-    }
+    return {'PROJECT_NAME': sc.PROJECT_NAME,
+            'BACKEND_SERVER_URL': sc.BACKEND_SERVER_URL}

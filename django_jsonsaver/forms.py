b06ca70efd6ea4451f2cd21419169415db170bf4
Nicholas Moen <arcanemachine@tutanota.com>
2021-02-27 15:05:53 -0700

    Add honeypot field to contact form

--- django_jsonsaver/forms.py
+++ django_jsonsaver/forms.py
@@ -7,6 +7,14 @@ from . import constants as c
 class ContactUsForm(forms.Form):
     name = forms.CharField(max_length=128)
     email = forms.EmailField()
+    # honeypot field
+    address = forms.CharField(widget=forms.HiddenInput(), required=False)
     message = forms.CharField(widget=forms.widgets.Textarea)
     captcha = CaptchaField(
         label="CAPTCHA", help_text=c.FORM_FIELD_CAPTCHA_HELP_TEXT)
+
+    def clean(self):
+        # honeypot
+        if self.cleaned_data.get('address', ''):
+            return False
+        return self.cleaned_data
